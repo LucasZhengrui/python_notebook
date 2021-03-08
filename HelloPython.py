84b2a7162f1b689ef01b346006dda64b5a31fc8f
LucasZhengrui <52554274+LucasZhengrui@users.noreply.github.com>
2021-03-08 15:29:26 +0800

    Create HelloPython.py

---
+++ HelloPython.py
@@ -0,0 +1,9 @@
+import math
+n = math.sin(0.5)
+print(n)
+n = math.cos(0.5)
+print(n)
+
+#runfile('C:/Users/h3cvdiuser/Desktop/untitled0.py', wdir='C:/Users/h3cvdiuser/Desktop')
+#0.479425538604203
+#0.8775825618903728
