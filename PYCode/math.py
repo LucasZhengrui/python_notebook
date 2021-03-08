f50abdf5b7597119406c7f60d5cc1e64a742f850
LucasZhengrui <52554274+LucasZhengrui@users.noreply.github.com>
2021-03-08 15:59:00 +0800

    Create math.py

---
+++ PYCode/math.py
@@ -0,0 +1,15 @@
+from math import *
+'''n = math.sin(3)                     #求正弦值
+print(n)
+n = math.gcd(36, 18)                #最大公约数
+print(n)
+n = math.pi                         #常数π
+print(n)
+n = math.e                          #常数e
+print(n)'''
+n=log2(8)                    #计算以2为底的对数值
+print(n)
+n=log10(100)                 #计算以10为底的对数值
+print(n)
+n=radians(180)               #把角度转换为弧度
+print(n)
