f42e03ac875828366a24de6ef0c70bba89c1ec59
LucasZhengrui <52554274+LucasZhengrui@users.noreply.github.com>
2021-03-08 15:53:30 +0800

    Create NumpyPack.py

---
+++ PYCode/NumpyPack.py
@@ -0,0 +1,6 @@
+import numpy as np             #导入扩展库numpy，并设置别名为np
+a = np.array([1,2,3,4])          
+print(a)
+
+#runfile('C:/Users/h3cvdiuser/Desktop/untitled0.py', wdir='C:/Users/h3cvdiuser/Desktop')
+#[1 2 3 4]
