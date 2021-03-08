6a03e68559adef96ae6c6d02f252be48fde799e2
LucasZhengrui <52554274+LucasZhengrui@users.noreply.github.com>
2021-03-08 15:46:01 +0800

    Create Random.py

---
+++ PYCode/Random.py
@@ -0,0 +1,9 @@
+import random                  #导入标准库random
+a = random.random()            #获得[0,1) 内的随机小数
+b = random.randint(1,100)      #获得[1,100]区间上的随机整数
+c = random.randrange(1, 100)   #返回[1, 100)区间中的随机整数
+print(a,b,c)
+
+
+#runfile('C:/Users/h3cvdiuser/Desktop/untitled0.py', wdir='C:/Users/h3cvdiuser/Desktop')
+#0.9418850641710088 51 42
